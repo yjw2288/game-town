fd29637ef2c83f63c75f9ec3e55ae2f059447784
yjw2288 <yjw2288@gmail.com>
2020-07-21 14:34:12 +0900

    * 쿠키 기반의 로그인으로 변경 * 로그아웃 추가

--- gt-api/src/main/java/com/gametown/api/account/AccountApiController.java
+++ gt-api/src/main/java/com/gametown/api/account/AccountApiController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/accounts")
 @AllArgsConstructor
@@ -18,17 +21,39 @@ public class AccountApiController {
     private final AccountLoginService accountLoginService;
 
     @PostMapping("/join")
-    public JoinView join(@RequestBody JoinFormDto joinForm) {
+    public JoinView join(@RequestBody JoinFormDto joinForm, HttpServletResponse response) {
         String sessionKey = accountJoinService.join(joinForm);
+
+        Cookie cookie = new Cookie("loginToken", sessionKey);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(-1);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
         return new JoinView(sessionKey);
     }
 
     @PostMapping("/login")
-    public LoginView login(@RequestBody LoginFormDto loginFormDto) {
+    public LoginView login(@RequestBody LoginFormDto loginFormDto, HttpServletResponse response) {
         String sessionKey =
                 accountLoginService.login(loginFormDto.getEmail(), loginFormDto.getPassword());
         LoginView loginView = new LoginView();
         loginView.setLoginToken(sessionKey);
+
+        Cookie cookie = new Cookie("loginToken", sessionKey);
+        cookie.setMaxAge(-1);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
         return loginView;
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("loginToken", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return "success";
     }
 }
